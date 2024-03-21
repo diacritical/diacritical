a1dbbb1b9573f9405813df4bc87c5b70d922305e
Jonathan Caprell <jbcaprell@diacritical.net>
2024-03-21 08:15:00 -0400

    Create `fly.io` deployment [`"diacritical"`]

--- mix.exs
+++ mix.exs
@@ -146,6 +146,7 @@ defmodule DiacriticalApp.MixProject do
       elixirc_paths: elixirc_paths(env),
       homepage_url: "https://diacritical.net",
       name: "Diacritical",
+      releases: [diacritical: [overlays: ["rel/overlay"]]],
       source_url: "https://github.com/diacritical/diacritical",
       start_permanent: env == :prod,
       version: "0.8.0"
