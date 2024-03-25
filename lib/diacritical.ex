3b461d1de5a98c1d7840510cbecfc93d38914f9f
Jonathan Caprell <jbcaprell@diacritical.net>
2024-03-25 07:30:00 -0400

    Create `Migration` wrapper

--- lib/diacritical.ex
+++ lib/diacritical.ex
@@ -3,7 +3,7 @@ defmodule Diacritical do
   @moduledoc since: "0.1.0"
 
   use Boundary,
-    deps: [Ecto, Ecto.Adapters.Postgres, Ecto.Adapters.SQL],
+    deps: [Ecto, Ecto.Adapters.Postgres, Ecto.Adapters.SQL, Ecto.Migrator],
     exports: [Supervisor]
 
   @typedoc "Represents the greeting."
