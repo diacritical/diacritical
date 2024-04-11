f26f8756886796727271674751f525b4e344bad9
Jonathan Caprell <jbcaprell@diacritical.net>
2024-04-11 07:00:00 -0400

    Update `MixProject` dependency list [`:floki`]

--- mix.exs
+++ mix.exs
@@ -160,6 +160,7 @@ defmodule DiacriticalApp.MixProject do
         {:ecto_sql, "~> 3.11"},
         {:esbuild, "~> 0.8", runtime: env == :dev},
         {:ex_doc, "~> 0.31", only: :dev, runtime: false},
+        {:floki, "~> 0.36", only: :test},
         {:gettext, "~> 0.24"},
         {
           :heroicons,
