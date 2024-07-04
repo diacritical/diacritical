feb9fbc9cba1237c290f99c765d514fa9d65c33b
Jonathan Caprell <jbcaprell@diacritical.net>
2024-07-03 20:30:00 -0400

    Update `MixProject` project

--- mix.exs
+++ mix.exs
@@ -181,7 +181,7 @@ defmodule DiacriticalApp.MixProject do
         {:phoenix_html, "~> 4.1"},
         {:phoenix_live_dashboard, "~> 0.8"},
         {:phoenix_live_reload, "~> 1.5", runtime: env == :dev},
-        {:phoenix_live_view, "1.0.0-rc.5", override: true},
+        {:phoenix_live_view, "1.0.0-rc.6", override: true},
         {:postgrex, "~> 0.18"},
         {:req, "~> 0.5"},
         {:swoosh, "~> 1.16"},
