04fed8b4c68dc00a088cfc2bb35da33155d20310
Jonathan Caprell <jbcaprell@diacritical.net>
2024-07-19 21:00:00 -0700

    Create `v0.45.0` release

--- mix.exs
+++ mix.exs
@@ -101,7 +101,7 @@ defmodule DiacriticalApp.MixProject do
       :diacritical
 
       iex> project()[:version]
-      "0.44.0"
+      "0.45.0"
 
   """
   @doc since: "0.1.0"
@@ -200,7 +200,7 @@ defmodule DiacriticalApp.MixProject do
       releases: [diacritical: [overlays: ["rel/overlay"]]],
       source_url: "https://github.com/diacritical/diacritical",
       start_permanent: env == :prod,
-      version: "0.44.0"
+      version: "0.45.0"
     ]
   end
 end
