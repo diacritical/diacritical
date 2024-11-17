7ae83584eaac166230ef276fc727e82687622c05
Jonathan Caprell <jbcaprell@diacritical.net>
2024-11-17 08:04:13 -0500

    Update `Endpoint` endpoint

--- config/dev.exs
+++ config/dev.exs
@@ -1,6 +1,18 @@
 import Config, only: [config: 2, config: 3, import_config: 1]
 
+alias DiacriticalWeb
+
+alias DiacriticalWeb.Endpoint
+
 import_config "test.exs"
+
+config :diacritical, Endpoint,
+  check_origin: false,
+  code_reloader: true,
+  debug_errors: true,
+  http: [port: 4_004],
+  https: [port: 4_005],
+  url: [host: "localhost", path: "/", port: 4_005, scheme: "https"]
 
 config :diacritical, env: [dev: true]
 config :logger, level: :debug
