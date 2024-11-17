192c3bc3ab2694136cf39400ff8ab32fbc4803b8
Jonathan Caprell <jbcaprell@diacritical.net>
2024-11-16 20:19:14 -0500

    Create `DiacriticalCase` boundary

---
+++ test/diacritical_case_test.exs
@@ -0,0 +1,10 @@
+defmodule DiacriticalCaseTest do
+  @moduledoc "Defines an `ExUnit.Case` case."
+  @moduledoc since: "0.3.0"
+
+  use ExUnit.Case, async: true
+
+  alias DiacriticalCase
+
+  doctest DiacriticalCase, import: true
+end
