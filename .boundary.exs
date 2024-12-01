b36df3a7a62c13c2a8fbb9839a30a4aa16ef3362
Jonathan Caprell <jbcaprell@diacritical.net>
2024-12-01 08:41:07 -0500

    Create `Endpoint` plug

--- .boundary.exs
+++ .boundary.exs
@@ -2,5 +2,5 @@
   Diacritical: [Diacritical, Diacritical.Supervisor],
   DiacriticalApp: [DiacriticalApp],
   DiacriticalCase: [DiacriticalCase, DiacriticalCase.Supervisor],
-  DiacriticalWeb: [DiacriticalWeb]
+  DiacriticalWeb: [DiacriticalWeb, DiacriticalWeb.Endpoint]
 ]
