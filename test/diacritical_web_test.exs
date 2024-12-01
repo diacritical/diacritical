a35befd63563b1a39c225c0dee29928ca5bf17d5
Jonathan Caprell <jbcaprell@diacritical.net>
2024-12-01 13:03:34 -0500

    Update `DiacriticalWeb` boundary [`static_path/0`]

---
+++ test/diacritical_web_test.exs
@@ -0,0 +1,18 @@
+defmodule DiacriticalWebTest do
+  @moduledoc "Defines an `ExUnit.Case` case."
+  @moduledoc since: "0.6.0"
+
+  use ExUnit.Case, async: true
+
+  alias DiacriticalWeb
+
+  doctest Diacritical, import: true
+
+  describe "static_path/0" do
+    import DiacriticalWeb, only: [static_path: 0]
+
+    test "success" do
+      assert static_path() == ~W[asset bimi.svg favicon.ico robots.txt]
+    end
+  end
+end
