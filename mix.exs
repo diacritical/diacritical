c30a9cb2f9c4657db6086672689ab3cd5808ad74
Jonathan Caprell <jbcaprell@diacritical.net>
2024-12-01 13:10:49 -0500

    Update `MixProject` dependency list [`:esbuild`]

--- mix.exs
+++ mix.exs
@@ -106,6 +106,7 @@ defmodule DiacriticalApp.MixProject do
         {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
         {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
         {:dns_cluster, "~> 0.1"},
+        {:esbuild, "~> 0.8", runtime: env == :dev},
         {:ex_doc, "~> 0.35", only: :dev, runtime: false},
         {:jason, "~> 1.4"},
         {:phoenix, "~> 1.7"},
