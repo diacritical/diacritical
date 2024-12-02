e8c0838ef75d58977a365d6f76de76ee892df88b
Jonathan Caprell <jbcaprell@diacritical.net>
2024-12-01 19:28:44 -0500

    Create `v0.8.0` release

--- mix.exs
+++ mix.exs
@@ -131,7 +131,7 @@ defmodule DiacriticalApp.MixProject do
       releases: [diacritical: [overlays: ["rel/overlay"]]],
       source_url: "https://github.com/diacritical/diacritical",
       start_permanent: env == :prod,
-      version: "0.7.0"
+      version: "0.8.0"
     ]
   end
 end
