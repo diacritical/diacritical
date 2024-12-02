dae7922db72813d915afa942b4905075a393fd20
Jonathan Caprell <jbcaprell@diacritical.net>
2024-12-01 20:57:09 -0500

    Update `Endpoint` endpoint

--- lib/diacritical_web/endpoint.ex
+++ lib/diacritical_web/endpoint.ex
@@ -56,6 +56,7 @@ defmodule DiacriticalWeb.Endpoint do
     socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
     plug Phoenix.CodeReloader
     plug Phoenix.LiveReloader
+    plug Phoenix.Ecto.CheckRepoStatus, otp_app: :diacritical
   end
 
   plug Plug.Parsers,
