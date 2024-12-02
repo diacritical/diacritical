a910cb259c2b7f840732233a2c3c9ee3150ba234
Jonathan Caprell <jbcaprell@diacritical.net>
2024-12-01 19:05:04 -0500

    Update `Endpoint` environment configuration [`:live_view`]

--- config/prod.exs
+++ config/prod.exs
@@ -25,6 +25,7 @@ config :diacritical, Endpoint,
     otp_app: :diacritical,
     port: 4_001
   ],
+  live_view: [signing_salt: "aTStPuSKpOqy8Elq"],
   pubsub_server: :"Elixir.Diacritical.PubSub",
   render_errors: [
     formats: [html: HTML.Error, txt: TXT.Error],
@@ -33,6 +34,12 @@ config :diacritical, Endpoint,
   ],
   secret_key_base:
     "rHHk51MXiuewZ3FpqbHMHso7zRMqFYN8sPGAVNJ0Y956svsgKntwfdrOKxM6WCMa",
+  session: [
+    key: "__Host-session",
+    same_site: "Strict",
+    signing_salt: "OGTrT2EZEe9HnTC5",
+    store: :cookie
+  ],
   server: true,
   url: [host: nil, path: "/", port: 443, scheme: "https"]
 
