9cd62eab69fcf3e085b7a8d7c1b5efe56a7b0d1f
Jonathan Caprell <jbcaprell@diacritical.net>
2024-12-02 19:47:31 -0500

    Create `CreateTableAccountToken` migration

--- priv/diacritical/repo/seed.exs
+++ priv/diacritical/repo/seed.exs
@@ -5,6 +5,8 @@ alias DiacriticalSchema.Account
 alias Diacritical.Repo
 alias DiacriticalSchema.TestSchema
 
+alias Account.Token
+
 if Mix.env() in [:dev, :test] and not Repo.exists?(TestSchema) do
   Repo.insert_all(TestSchema, Enum.map(1_001..2_000, &%{x: :rand.uniform(&1)}))
 end
@@ -19,6 +21,24 @@ if Mix.env() in [:dev, :test] and not Repo.exists?(Account) do
           confirmed_at: DateTime.add(DateTime.utc_now(), 15),
           email: "jdoe+#{System.unique_integer([:positive])}@example.com",
           password_digest: Argon2.hash_pwd_salt("correct horse battery staple")
+        }
+      end
+    )
+  )
+end
+
+if Mix.env() in [:dev, :test] and not Repo.exists?(Token) do
+  account = Repo.all(Account)
+
+  Repo.insert_all(
+    Token,
+    Enum.map(
+      account,
+      fn entry ->
+        %{
+          account_id: entry.id,
+          data: :crypto.strong_rand_bytes(32),
+          type: "session"
         }
       end
     )
