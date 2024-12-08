ad27b037ebdd96da976d4aecc68db83754e358c6
Jonathan Caprell <jbcaprell@diacritical.net>
2024-12-08 09:38:58 -0500

    Create `Error` text template

---
+++ lib/diacritical_web/txt/error.ex
@@ -0,0 +1,8 @@
+defmodule DiacriticalWeb.TXT.Error do
+  @moduledoc "Defines a `Phoenix.Template` template set."
+  @moduledoc since: "0.5.0"
+
+  use DiacriticalWeb.TXT
+
+  embed_templates "error/*"
+end
