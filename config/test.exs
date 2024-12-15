98f8a54f1fbf1aa59c08ef19652f9ced77cd47fe
Jonathan Caprell <jbcaprell@diacritical.net>
2024-12-14 20:36:31 -0500

    Create `:argon2_elixir` environment configuration

--- config/test.exs
+++ config/test.exs
@@ -9,6 +9,8 @@ alias DiacriticalWeb.Endpoint
 
 import_config "prod.exs"
 
+config :argon2_elixir, m_cost: 8, t_cost: 1
+
 config :diacritical, Endpoint,
   cache_static_manifest: nil,
   http: [port: 4_002],
