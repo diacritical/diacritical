1b8f7f4aaf4c7dbf161b2090bd2357295e4edcf1
Jonathan Caprell <jbcaprell@diacritical.net>
2024-12-14 20:34:48 -0500

    Update `MixProject` dependency list [`:argon2_elixir`]

--- mix.exs
+++ mix.exs
@@ -141,6 +141,7 @@ defmodule DiacriticalApp.MixProject do
       boundary: [default: [type: :strict]],
       compilers: [:boundary | Mix.compilers()],
       deps: [
+        {:argon2_elixir, "~> 4.1"},
         {:bandit, "~> 1.6"},
         {:boundary, "~> 0.10", runtime: false},
         {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
