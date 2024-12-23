4df8af45ae090001f9a42cc2df3820ae0df2b910
Jonathan Caprell <jbcaprell@diacritical.net>
2024-12-22 20:30:27 -0500

    Create `"prettier"` configuration

--- mix.exs
+++ mix.exs
@@ -110,6 +110,8 @@ defmodule DiacriticalApp.MixProject do
       aliases: [
         "asset.build": "esbuild diacritical_web",
         "asset.deploy": ["esbuild diacritical_web --minify", "phx.digest"],
+        "asset.format": ["cmd --cd #{@asset_path} npm run format"],
+        "asset.format.check": ["cmd --cd #{@asset_path} npm run format:check"],
         "asset.setup": ["npm.install", "esbuild.install --if-missing"],
         "boundary.ex_doc_groups": [
           "boundary.ex_doc_groups",
