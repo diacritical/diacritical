421912e5ecb1f4212749c080f340f0a5d7520f42
Jonathan Caprell <jbcaprell@diacritical.net>
2025-01-01 17:37:07 -0500

    Create `v0.2.0` release

--- mix.exs
+++ mix.exs
@@ -73,7 +73,7 @@ defmodule Diacritical.MixProject do
       name: "Diacritical",
       source_url: "https://github.com/diacritical/diacritical",
       start_permanent: env == :prod,
-      version: "0.1.0"
+      version: "0.2.0"
     ]
   end
 end
