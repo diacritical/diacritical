f2abc5084a6a3424f06a2ab4a5b7c67a9a029677
Jonathan Caprell <jbcaprell@diacritical.net>
2025-01-26 19:00:37 -0500

    Create `DiacriticalApp` application

--- mix.exs
+++ mix.exs
@@ -1,8 +1,20 @@
-defmodule Diacritical.MixProject do
-  @moduledoc "Defines a `Mix.Project` project."
+defmodule DiacriticalApp.MixProject do
+  @moduledoc "Defines a `Mix.Project` project and an OTP application."
   @moduledoc since: "0.1.0"
 
   use Mix.Project
+
+  alias DiacriticalApp
+
+  @typedoc "Represents the application configuration keyword."
+  @typedoc since: "0.3.0"
+  @type application_keyword() ::
+          {:mod, {module(), DiacriticalApp.init()}}
+          | {Keyword.key(), Keyword.value()}
+
+  @typedoc "Represents the application configuration."
+  @typedoc since: "0.3.0"
+  @type application() :: [application_keyword()]
 
   @typedoc "Represents the environment."
   @typedoc since: "0.2.0"
@@ -19,7 +31,7 @@ defmodule Diacritical.MixProject do
   @typedoc "Represents the project configuration keyword."
   @typedoc since: "0.1.0"
   @type project_keyword() ::
-          {:app, Application.app()}
+          {:app, DiacriticalApp.app()}
           | {:version, String.t()}
           | {Keyword.key(), Keyword.value()}
 
@@ -40,6 +52,23 @@ defmodule Diacritical.MixProject do
   defp elixirc_paths(:dev), do: elixirc_paths(:test)
   defp elixirc_paths(:test), do: ["support" | elixirc_paths(:prod)]
   defp elixirc_paths(env) when is_atom(env), do: ["lib"]
+
+  @doc """
+  Returns the application configuration.
+
+  ## Example
+
+      iex> {DiacriticalApp, _init} = application()[:mod]
+
+  """
+  @doc since: "0.3.0"
+  @spec application() :: application()
+  def application() do
+    [
+      extra_applications: [:logger, :os_mon, :runtime_tools],
+      mod: {DiacriticalApp, []}
+    ]
+  end
 
   @doc """
   Returns the project configuration.
