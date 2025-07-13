d27f828e1131944b52f7ffd7526775b46d2bdcf7
Jonathan Caprell <jbcaprell@diacritical.net>
2025-07-13 13:43:07 -0400

    Create `Credo` configuration [`"default"`]

--- mix.exs
+++ mix.exs
@@ -33,7 +33,8 @@ defmodule Diacritical.MixProject do
           "boundary.ex_doc_groups",
           "cmd sed 1d boundary.exs > .boundary.exs",
           "cmd rm boundary.exs"
-        ]
+        ],
+        credo: "credo --config-name default"
       ],
       app: :diacritical,
       boundary: [default: [type: :strict]],
