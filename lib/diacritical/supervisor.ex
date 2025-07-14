fc65eeb9e4a0b92181b394d4024f54600e06fe9f
Jonathan Caprell <jbcaprell@diacritical.net>
2025-07-14 19:27:56 -0400

    Create `Supervisor` supervisor

---
+++ lib/diacritical/supervisor.ex
@@ -0,0 +1,109 @@
+defmodule Diacritical.Supervisor do
+  @moduledoc "Defines a `Supervisor` supervisor."
+  @moduledoc since: "0.3.0"
+
+  use Supervisor
+
+  @typedoc "Represents the initialization."
+  @typedoc since: "0.3.0"
+  @type init() :: Keyword.t()
+
+  @typedoc "Represents the child specification."
+  @typedoc since: "0.3.0"
+  @type child_spec() :: Supervisor.child_spec()
+
+  @typedoc "Represents the initialization confirmation."
+  @typedoc since: "0.3.0"
+  @type on_init() :: :ignore | {:ok, {Supervisor.sup_flags(), [child_spec()]}}
+
+  @typedoc "Represents the option keyword."
+  @typedoc since: "0.3.0"
+  @type opt_keyword() ::
+          {:name, Supervisor.name()} | {Keyword.key(), Keyword.value()}
+
+  @typedoc "Represents the option."
+  @typedoc since: "0.3.0"
+  @type opt() :: [opt_keyword()]
+
+  @typedoc "Represents the start process confirmation."
+  @typedoc since: "0.3.0"
+  @type on_start() :: Supervisor.on_start()
+
+  @doc """
+  Defines the child specification for the given `init`.
+
+  ## Example
+
+      iex> %{init: %{valid: init}} = c_init(%{})
+      iex>
+      iex> child_spec(init)
+      %{
+        id: Supervisor,
+        start: {Supervisor, :start_link, [init]},
+        type: :supervisor
+      }
+
+  """
+  @doc since: "0.3.0"
+  @spec child_spec(init()) :: child_spec()
+  def child_spec(init) when is_list(init) do
+    %{
+      id: __MODULE__,
+      start: {__MODULE__, :start_link, [init]},
+      type: :supervisor
+    }
+  end
+
+  @impl Supervisor
+  @doc """
+  Initializes a supervisor process with the given `init`.
+
+  ## Example
+
+      iex> %{init: %{valid: init}} = c_init(%{})
+      iex>
+      iex> init(init)
+      {
+        :ok,
+        {
+          %{
+            auto_shutdown: :never,
+            intensity: 3,
+            period: 5,
+            strategy: :one_for_one
+          },
+          init
+        }
+      }
+
+  """
+  @doc since: "0.3.0"
+  @spec init(init()) :: on_init()
+  def init(init) when is_list(init) do
+    Supervisor.init(init[:children] || [], strategy: :one_for_one)
+  end
+
+  @doc """
+  Starts a supervisor with the given `init` and `opt`.
+
+  If an `opt` list is not given, the function will give the process this module
+  to use as its locally registered name.
+
+  ## Example
+
+      iex> start_supervisor!(%{})
+      iex> %{init: %{valid: init}} = c_init(%{})
+      iex> %{err: err} = c_err(%{})
+      iex>
+      iex> start_link(init)
+      err
+
+  """
+  @doc since: "0.3.0"
+  @spec start_link(init()) :: on_start()
+  @spec start_link(init(), opt()) :: on_start()
+  def start_link(init, opt \\ [name: __MODULE__])
+      when is_list(init) and is_list(opt) do
+    Supervisor.start_link(__MODULE__, init, opt)
+  end
+end
---
+++ test/diacritical/supervisor_test.exs
@@ -0,0 +1,121 @@
+defmodule Diacritical.SupervisorTest do
+  @moduledoc "Defines an `ExUnit.Case` case."
+  @moduledoc since: "0.3.0"
+
+  use ExUnit.Case, async: true
+
+  alias Diacritical
+
+  alias Diacritical.Supervisor
+
+  @typedoc "Represents the context."
+  @typedoc since: "0.3.0"
+  @type context() :: map()
+
+  @typedoc "Represents the context merge value."
+  @typedoc since: "0.3.0"
+  @type context_merge() ::
+          :ok | {:ok, map() | Keyword.t()} | map() | Keyword.t()
+
+  @spec c_init(context()) :: context_merge()
+  defp c_init(c) when is_map(c), do: %{init: %{invalid: %{}, valid: []}}
+
+  @spec start_supervisor!(context()) :: context_merge()
+  defp start_supervisor!(c) when is_map(c) do
+    start_supervised!(Supervisor)
+    :ok
+  end
+
+  @spec c_err(context()) :: context_merge()
+  defp c_err(c) when is_map(c) do
+    %{err: {:error, {:already_started, Process.whereis(Supervisor)}}}
+  end
+
+  doctest Supervisor, import: true
+
+  describe "child_spec/1" do
+    import Supervisor, only: [child_spec: 1]
+
+    setup :c_init
+
+    test "FunctionClauseError", %{init: %{invalid: init}} do
+      assert_raise FunctionClauseError, fn -> child_spec(init) end
+    end
+
+    test "success", %{init: %{valid: init}} do
+      assert child_spec(init) == %{
+               id: Supervisor,
+               start: {Supervisor, :start_link, [init]},
+               type: :supervisor
+             }
+    end
+  end
+
+  describe "init/1" do
+    import Supervisor, only: [init: 1]
+
+    setup :c_init
+
+    test "FunctionClauseError", %{init: %{invalid: init}} do
+      assert_raise FunctionClauseError, fn -> init(init) end
+    end
+
+    test "success", %{init: %{valid: init}} do
+      assert init(init) == {
+               :ok,
+               {
+                 %{
+                   auto_shutdown: :never,
+                   intensity: 3,
+                   period: 5,
+                   strategy: :one_for_one
+                 },
+                 init
+               }
+             }
+    end
+  end
+
+  describe "start_link/1" do
+    import Supervisor, only: [start_link: 1]
+
+    setup ~W[start_supervisor! c_init c_err]a
+
+    test "FunctionClauseError", %{init: %{invalid: init}} do
+      assert_raise FunctionClauseError, fn -> start_link(init) end
+    end
+
+    test "success", %{err: err, init: %{valid: init}} do
+      assert start_link(init) == err
+    end
+  end
+
+  describe "start_link/2" do
+    import Supervisor, only: [start_link: 2]
+
+    setup ~W[start_supervisor! c_init c_err]a
+    setup do: %{opt: %{default: [name: Supervisor], empty: [], invalid: %{}}}
+
+    test "FunctionClauseError (&1)", %{
+      init: %{invalid: init},
+      opt: %{default: opt}
+    } do
+      assert_raise FunctionClauseError, fn -> start_link(init, opt) end
+    end
+
+    test "FunctionClauseError (&2)", %{
+      init: %{valid: init},
+      opt: %{invalid: opt}
+    } do
+      assert_raise FunctionClauseError, fn -> start_link(init, opt) end
+    end
+
+    test "default", %{err: err, init: %{valid: init}, opt: %{default: opt}} do
+      assert start_link(init, opt) == err
+    end
+
+    test "empty", %{init: %{valid: init}, opt: %{empty: opt}} do
+      assert {:ok, _pid} = start_link(init, opt)
+    end
+  end
+end
