92d4f5fb238339d4e30d0352fb2d87204f2166f6
Jonathan Caprell <jbcaprell@diacritical.net>
2025-07-14 19:39:19 -0400

    Create `DiacriticalCase` boundary

---
+++ support/diacritical_case.ex
@@ -0,0 +1,46 @@
+defmodule DiacriticalCase do
+  @moduledoc "Defines commonalities for `ExUnit.CaseTemplate` case templates."
+  @moduledoc since: "0.3.0"
+
+  use Boundary, deps: [ExUnit]
+
+  @typedoc "Represents the option."
+  @typedoc since: "0.3.0"
+  @type opt() :: Keyword.t()
+
+  @typedoc "Represents the abstract syntax tree."
+  @typedoc since: "0.3.0"
+  @type macro() :: Macro.t()
+
+  @typedoc "Represents the context."
+  @typedoc since: "0.3.0"
+  @type context() :: map()
+
+  @typedoc "Represents the context merge value."
+  @typedoc since: "0.3.0"
+  @type context_merge() ::
+          :ok | {:ok, map() | Keyword.t()} | map() | Keyword.t()
+
+  @doc """
+  In `use`, calls `use ExUnit.CaseTemplate`.
+
+  ## Example
+
+      iex> defmodule TestTemplate do
+      ...>   use DiacriticalCase
+      ...> end
+      iex>
+      iex> TestTemplate.__ex_unit__(:setup, %{})
+      %{}
+      iex> TestTemplate.__ex_unit__(:setup_all, %{})
+      %{}
+
+  """
+  @doc since: "0.3.0"
+  @spec __using__(opt()) :: macro()
+  defmacro __using__(opt) when is_list(opt) do
+    quote do
+      use ExUnit.CaseTemplate, unquote(opt)
+    end
+  end
+end
