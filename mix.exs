bba4d29a066d5381e5c3ec5a38e92bb0cd377bdd
Jonathan Caprell <jbcaprell@diacritical.net>
2025-07-20 14:29:13 -0400

    Update `MixProject` dependency list [`:phoenix`]

--- mix.exs
+++ mix.exs
@@ -102,7 +102,8 @@ defmodule DiacriticalApp.MixProject do
         {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
         {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
         {:dns_cluster, "~> 0.2"},
-        {:ex_doc, "~> 0.38", only: :dev, runtime: false}
+        {:ex_doc, "~> 0.38", only: :dev, runtime: false},
+        {:phoenix, "~> 1.7"}
       ],
       deps_path: "dep",
       dialyzer: [ignore_warnings: ".dialyzer.exs", plt_add_apps: [:ex_unit]],
