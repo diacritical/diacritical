e1ae9afc5b01f7a12277e843c98919800b13274b
Jonathan Caprell <jbcaprell@diacritical.net>
2025-07-20 22:06:24 -0400

    Create `Error` text template

--- config/prod.exs
+++ config/prod.exs
@@ -3,6 +3,7 @@ import Config, only: [config: 2, config: 3]
 alias DiacriticalWeb
 
 alias DiacriticalWeb.Endpoint
+alias DiacriticalWeb.TXT
 
 config :diacritical, Endpoint,
   adapter: Bandit.PhoenixAdapter,
@@ -23,7 +24,7 @@ config :diacritical, Endpoint,
     port: 4_001
   ],
   pubsub_server: :"Elixir.Diacritical.PubSub",
-  render_errors: nil,
+  render_errors: [formats: [txt: TXT.Error]],
   secret_key_base:
     "TMK8Q5EFfQlxa6k4bt02uplsaTf0gDLu0O8B3F8HukfaPXqBIgoXZVURXIbAMKLn",
   server: true,
---
+++ test/diacritical_web/txt/error_test.exs
@@ -0,0 +1,49 @@
+defmodule DiacriticalWeb.TXT.ErrorTest do
+  @moduledoc "Defines an `ExUnit.Case` case."
+  @moduledoc since: "0.5.0"
+
+  use DiacriticalCase.View, async: true
+
+  alias DiacriticalWeb
+
+  alias DiacriticalWeb.TXT
+
+  alias TXT.Error
+
+  @typedoc "Represents the context."
+  @typedoc since: "0.5.0"
+  @type context() :: DiacriticalCase.context()
+
+  @typedoc "Represents the context merge value."
+  @typedoc since: "0.5.0"
+  @type context_merge() :: DiacriticalCase.context_merge()
+
+  @spec c_assigns_empty(context()) :: context_merge()
+  defp c_assigns_empty(c) when is_map(c), do: %{assigns: %{}}
+
+  describe "__mix_recompile__?/0" do
+    import Error, only: [__mix_recompile__?: 0]
+
+    test "failure" do
+      refute __mix_recompile__?()
+    end
+  end
+
+  describe "404/1" do
+    setup :c_assigns_empty
+
+    test "success", %{assigns: assigns} do
+      assert function_exported?(Error, :"404", 1)
+      assert Error."404"(assigns) == "Not Found\n"
+    end
+  end
+
+  describe "500/1" do
+    setup :c_assigns_empty
+
+    test "success", %{assigns: assigns} do
+      assert function_exported?(Error, :"500", 1)
+      assert Error."500"(assigns) == "Internal Server Error\n"
+    end
+  end
+end
